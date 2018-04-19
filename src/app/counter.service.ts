5190ce1a0964635641337c6cc9a957b589853698
DavidWallJ <djkindread@gmail.com>
2018-04-19 11:31:35 +0800

    first commit

--- src/app/counter.service.ts
+++ src/app/counter.service.ts
@@ -1,12 +1,6 @@
-import { Injectable } from '@angular/core';
-import { UserService } from './user.service';
-
-@Injectable()
 export class CounterService {
   activeCount = 0;
   inactiveCount = 0;
-
-  constructor(private userService: UserService) {}
 
   activeToInactive() {
     this.inactiveCount++;
